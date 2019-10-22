8be90efee3ab4bdfcf12e02eb49c795f240637ee
rayanabdirahman <rayanmabdirahman@gmail.com>
2019-10-22 19:38:39 +0100

    Protect category create route for admins

--- src/api/category.route.ts
+++ src/api/category.route.ts
@@ -1,10 +1,22 @@
 import { Router } from 'express';
 import CategoryController from '../controllers/category/category.controller';
+import AuthController from '../controllers/auth/auth.controller';
+import UserController from '../controllers/user/user.controller';
 
 const router = Router();
 const categoryController = new CategoryController();
+const authController = new AuthController(); // TODO: MOVE RESUED AUTH METHODS INTO MIDDLEWARE FOLDER
+const userController = new UserController(); // TODO: MOVE RESUED USERID METHOD INTO MIDDLEWARE FOLDER
 
-router.post('/create', categoryController.create);
+// Authorised Admin user can only create categories
+router.post('/create/:userId',
+  authController.authGuard,
+  authController.isAuth,
+  authController.isAdmin,
+  categoryController.create
+);
+
+router.param('userId', userController.findById);
 
 
 export default router;
