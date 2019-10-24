6b4c9497823377426949b99bc0b6d9044050980e
rayanabdirahman <rayanmabdirahman@gmail.com>
2019-10-24 18:13:06 +0100

    User cors package

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import * as swaggerUi from 'swagger-ui-express';
+import * as YAML from 'yamljs';
+import cors from 'cors';
 import auth from './api/auth.route';
 import user from './api/user.route';
 import category from './api/category.route';
 import product from './api/product.route';
 import connectToDbClient from './data_access/db_client';
-import * as swaggerUi from 'swagger-ui-express';
-import * as YAML from 'yamljs';
 
 const app = express();
 const SWAGGER_YAML_FILE_PATH = 'swagger/api-v1.yaml';
@@ -16,6 +17,7 @@ connectToDbClient(); // connect to mongo database
 // use middleware
 app.use(express.json()); // use express body parser to access req.body
 app.use(cookieParser()); // use express cookie parser to parse cookie header and populate req.cookies
+app.use(cors()); // user cors to enable CORS
 
 // Swagger configuration for API documentation
 const swaggerOptions = {
