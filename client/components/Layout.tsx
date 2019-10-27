d9c2db551c46a41d66f7eb8d55f110d035930d77
rayanabdirahman <rayanmabdirahman@gmail.com>
2019-10-27 19:37:19 +0000

    Add bootstrap boilerplate for navbar component

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import Link from 'next/link'
 import Head from 'next/head'
+import Navbar from './Navbar'
 
 type Props = {
   title?: string
@@ -16,30 +16,7 @@ const Layout: React.FunctionComponent<Props> = ({
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <nav>
-        <img src="https://getbootstrap.com/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" alt=""/>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/login">
-          <a>Login</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/signup">
-          <a>Signup</a>
-        </Link>
-      </nav>
-    </header>
+    <Navbar />
     {children}
     <footer>
       <hr />
--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -3,7 +3,20 @@ import Link from 'next/link'
 
 const Navbar: React.FunctionComponent = () => (
   <header>
-    <nav>
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <div className="container">
+        {/* Brand logo */}
+        <Link href="/">
+          <a className="navbar-brand">Navbar</a>
+        </Link>
+        {/* Brand logo */}
+
+        {/* Search bar */}
+        <div className="input-group-overlay">
+          <input type="text" placeholder="Search for products" className="form-control appended-form-control"/>
+        </div>
+        {/* Search bar */}
+      </div>
       <img src="https://getbootstrap.com/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" alt=""/>
       <Link href="/">
         <a>Home</a>
