e699346cfa8178e324bfb4162ab9663e504c53b0
rayanabdirahman <rayanmabdirahman@gmail.com>
2019-11-01 22:42:45 +0000

    Add admin dashboard page

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -5,6 +5,7 @@ import SignupPage from './pages/auth/Signup';
 import HomePage from './pages/core/Home';
 import PrivateRoute from './PrivateRoute';
 import UserDashboardPage from './pages/user/Dashboard';
+import AdminDashboardPage from './pages/admin/Dashboard';
 
 const Routes = () => {
   return (
@@ -14,6 +15,7 @@ const Routes = () => {
         <Route path="/login" exact component={LoginPage}></Route>
         <Route path="/signup" exact component={SignupPage}></Route>
         <PrivateRoute path="/user/dashboard" exact component={UserDashboardPage} />
+        <PrivateRoute path="/admin/dashboard" exact component={AdminDashboardPage} />
       </Switch>
     </BrowserRouter>
   )
---
+++ client/src/pages/admin/Dashboard.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+import { isAuthenticated } from '../../api/auth';
+import { Link } from 'react-router-dom';
+
+const AdminDashboardPage: React.FunctionComponent = () => {
+  // destructure user information from isAuthenticated response
+  const {user} = isAuthenticated()
+  const name = `${user.firstName} ${user.lastName}`
+
+  const adminLinks = () => (
+    <div className="card mb-5">
+      <h3 className="card-header">Admin Links</h3>
+      <ul className="list-group">
+        <li className="list-group-item"> <Link to="/create/category">Create Category</Link></li>
+        <li className="list-group-item"><Link to="/create/product">Create Product</Link></li>
+      </ul>
+    </div>
+  )
+
+  const adminInfo = () => (
+    <div className="card mb-5">
+      <h3 className="card-header">Admin Information</h3>
+      <ul className="list-group">
+        <li className="list-group-item">name: {user ? name : null}</li>
+        <li className="list-group-item">email: {user ? user.email : null}</li>
+        <li className="list-group-item">role: {user ? user.role === 1 ? 'Admin' : 'Registered USer' : null}</li>
+      </ul>
+    </div>
+  )
+
+  return (
+    <Layout title="User Dashboard" description={`Welcome back ${name}`} >
+      <div className="row">
+        <div className="col-3">
+          {adminLinks()}
+        </div>
+        <div className="col-9">
+          {adminInfo()}
+        </div>
+      </div>
+
+      
+    </Layout>
+  )
+}
+
+export default AdminDashboardPage;
