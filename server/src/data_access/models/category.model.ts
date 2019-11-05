23d97268f46c0137f1828cca935ddf34d83d3cff
rayanabdirahman <rayanmabdirahman@gmail.com>
2019-11-05 19:05:28 +0000

    Ensure category name is unique

--- server/src/data_access/models/category.model.ts
+++ server/src/data_access/models/category.model.ts
@@ -2,7 +2,7 @@ import { ICategory } from './interfaces';
 import mongoose from 'mongoose';
 
 const CategorySchema = new mongoose.Schema({
-  name: { type: String, trim: true, required: true, maxlength: 32 },
+  name: { type: String, trim: true, required: true, maxlength: 32, unique: true },
 }, { timestamps: true });
 
 export default mongoose.model<ICategory>('Category', CategorySchema);
