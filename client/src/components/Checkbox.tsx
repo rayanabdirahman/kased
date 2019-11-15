d616708437e72d0a33f34a6e7ae72c79a6969454
rayanabdirahman <rayanmabdirahman@gmail.com>
2019-11-15 18:37:14 +0000

    Handle checkbox toggle

--- client/src/components/Checkbox.tsx
+++ client/src/components/Checkbox.tsx
@@ -4,13 +4,37 @@ interface IProps {
   categories: any
 }
 
-const Checkbox: React.FunctionComponent<IProps> = ({ categories }) => (
-  categories.map((category: any, index: number) => (
-    <li key={`category-checkbox--${index}`} className={`list-unstyled`}>
-      <input type="checkbox" className="fomr-check-input"/>
-      <label className="form-check-label">{category.name}</label>
-    </li>
-  ))
-)
+const Checkbox: React.FunctionComponent<IProps> = ({ categories }) => {
+  const [checked, setChecked] = React.useState<any>([])
+
+  const handleToggle = (categoryId: any) => () => {
+    // check if checked category has been registred to state already by searching for category Id
+    const currentCategoryId = checked.indexOf(categoryId) // return -1 if categoryId does not exist in state
+    const newCheckedCategoryId = [...checked]
+
+    // push new checked category into state if it is not there already
+    if (currentCategoryId === -1) {
+      newCheckedCategoryId.push(categoryId)
+    } else {
+      // else remove category from state if it is there
+      newCheckedCategoryId.splice(currentCategoryId, 1)
+    }
+
+    console.log(newCheckedCategoryId)
+
+    // set state with new checked items
+    setChecked(newCheckedCategoryId)
+  }
+
+  return (
+    categories.map((category: any, index: number) => (
+      <li key={`category-checkbox--${index}`} className={`list-unstyled`}>
+        <input onChange={handleToggle(category._id)} value={checked.indexOf(category._id === -1)} type="checkbox" className="fomr-check-input"/>
+        <label className="form-check-label">{category.name}</label>
+      </li>
+    ))
+  )
+}
+
 
 export default Checkbox
