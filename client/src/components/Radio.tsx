28f913f103f473c3719ad8a866ce7b4a9eb339e9
rayanabdirahman <rayanmabdirahman@gmail.com>
2019-11-17 21:35:24 +0000

    Remove commented out functions

--- client/src/components/Radio.tsx
+++ client/src/components/Radio.tsx
@@ -8,26 +8,6 @@ interface IProps {
 const Radio: React.FunctionComponent<IProps> = ({ prices, handleFilters }) => {
   const [value, setValue] = React.useState<any>(0)
 
-  // const handleToggle = (categoryId: any) => () => {
-  //   // check if checked category has been registred to state already by searching for category Id
-  //   const currentCategoryId = checked.indexOf(categoryId) // return -1 if categoryId does not exist in state
-  //   const newCheckedCategoryId = [...checked]
-
-  //   // push new checked category into state if it is not there already
-  //   if (currentCategoryId === -1) {
-  //     newCheckedCategoryId.push(categoryId)
-  //   } else {
-  //     // else remove category from state if it is there
-  //     newCheckedCategoryId.splice(currentCategoryId, 1)
-  //   }
-
-  //   // set state with new checked items
-  //   setChecked(newCheckedCategoryId)
-
-  //   // send checked filters to parent component
-  //   handleFilters(newCheckedCategoryId)
-  // }
-
   const handleChange = (event: any) => {
     // send onChange value to parent component
     handleFilters(event.target.value)
