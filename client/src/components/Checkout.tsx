e3c5e6c0a1869d889cc84fad3c85ac9f4eb6e374
rayanabdirahman <rayanmabdirahman@gmail.com>
2019-11-22 20:31:23 +0000

    Remove imports from checkout component that are not required

--- client/src/components/Checkout.tsx
+++ client/src/components/Checkout.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { getCartItems } from '../api/cart';
-import Card from './Card';
 import { Link } from 'react-router-dom';
-import Layout from './Layout';
 import { isAuthenticated } from '../api/auth';
 
 interface IProps {
