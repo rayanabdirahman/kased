fbd4031d939b52259a03d09381a0ac13a55b67d7
rayanabdirahman <rayanmabdirahman@gmail.com>
2019-11-27 22:52:58 +0000

    Update profuctImage component

--- client/src/components/ProductImage.tsx
+++ client/src/components/ProductImage.tsx
@@ -9,7 +9,7 @@ interface IProps {
 const ProductImage: React.FunctionComponent<IProps> = ({ item, url}) => {
   return (
     <div className="product-image">
-      <img src={`${API_BASE_URL}/${url}/photo/${item._id}`} alt={item.name} className="mb-3" style={{ maxHeight: '100%', maxWidth: '100%'}} />
+      <img src={`${API_BASE_URL}/${url}/photo/${item._id}`} alt={item.name} className="mb-3"/>
     </div>
   );
 }
