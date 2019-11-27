6d1a90ab704e447b2685bb7dd00a0f7d482faa7f
rayanabdirahman <rayanmabdirahman@gmail.com>
2019-11-27 17:26:04 +0000

    Update primary button styles

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -12,6 +12,10 @@ const Hero: React.FunctionComponent<Props> = ({ title, description }) => (
       <p className="lead">{description}</p> */}
       <h1 className="display-4 hero-header">Bulk shopping made easy</h1>
       <p className="lead hero-lead">Shop all your favourite brands, in bulk, and we’ll deliver them straight to your door</p>
+
+      {/* TODO: Update when modal for login & signup is enabled */}
+      <button className="btn btn-primary btn-lg">Login</button>
+      <button className="btn btn-primary btn-lg">Sign up</button>
     </div>
   </div>
 )
